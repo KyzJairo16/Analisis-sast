2e5ee69cdc2a871397326c7eb97c6e528184b8ca
Jairo Esteban <jairodavid.estebanlozano@gmail.com>
2026-05-13 12:34:00 -0500

    Cambio
    s

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { AdministradorService } from '../services/login.service';
+
 
 
 
@@ -11,26 +11,4 @@ import { AdministradorService } from '../services/login.service';
   styleUrl: './login.css',
 })
 export class Login {
-  credenciales = {
-    usuario: '',
-    contrasenia: ''
-  };
-
-  private router = inject(Router);
-  private adminService = inject(AdministradorService);
-
-  iniciarSesion() {
-    this.adminService.login(this.credenciales.usuario, this.credenciales.contrasenia).subscribe({
-
-      next: (respuesta) => {
-        console.log( respuesta);
-        this.router.navigate(['/administrador']);
-      },
-
-      error: (err) => {
-        console.error(err.error || 'Error de acceso');
-      }
-
-    });
-  }
 }
